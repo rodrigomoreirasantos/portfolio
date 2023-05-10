4369483f24ee83f8601882ea23a7e2c71f1b3f10
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2023-05-10 15:18:46 -0400

    responsive Experience

--- src/components/ExperienceCards.tsx
+++ src/components/ExperienceCards.tsx
@@ -17,37 +17,37 @@ export default function ExperienceCards({experience}: Props) {
     return (
     <article 
         className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[300px] md:w-[400px] xl:w-[700px] 
-        snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-300 overflow-hidden'
+        snap-center bg-[#292929] p-10 xs:p-5 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-300 overflow-hidden'
     >
         <motion.img
-            initial={{ y: -100, opacity: 0 }}
+            initial={{ y: -50, opacity: 0 }}
             transition={{ duration: 1.2 }}
             whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true }}
-            className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'
+            // viewport={{ once: true }}
+            className='w-32 h-32 rounded-full xs:w-16 xs:h-16 lg:w-20 lg:h-20 xl:w-[200px] xl:h-[200px] object-cover object-center'
             src={urlFor(experience?.companyImage).url()}
-            alt="Geopixel" 
+            alt={experience.company}
         />
 
         <div className='px-0 md:px-10'>
-            <h4 className='text-4xl font-light'>Developer</h4>
-            <p className='font-bold text-2xl mt-1'>Geopixel</p>
+            <h4 className='text-base md:text-2xl font-light'>{experience.jobTitle}</h4>
+            <p className='font-bold text-base md:text-xl mt-1'>{experience.company}</p>
             <div className='flex space-x-2 my-2'>
                 {experience.technologies.map(technology => (
                     <img 
                         key={technology._id}
                         src={urlFor(technology.image).url()}
-                       className="h-10 w-10 rounded-full"
+                       className="h-10 w-10 md:h-6 md:w-6 xs:h-5 xs:w-5 rounded-full"
                     />
                 ))}
             </div>
 
-            <p className='uppercase py-5 text-gray-300'>
+            <p className='uppercase py-5 text-gray-300 xs:py-1 xs:text-xs lg:text-base'>
                 {new Intl.DateTimeFormat('en-US', {year: "numeric", month: "long"}).format(dateStarted)} - {" "}
                 {experience.isCurrentlyWorkingHere ? "Present" : new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'long'}).format(dateEnded)}
             </p>
 
-            <ul className='list-disc space-y-4 ml-5 text-lg overflow-y-hidden'>
+            <ul className='list-disc space-y-4 ml-5 lg:text-base overflow-y-hidden xs:space-y-1 xs:text-xs'>
                 {experience.points.map((point, index) => (
                     <li key={index}>{point}</li>
                 ))}
