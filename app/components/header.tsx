a95631fe6d073ad6ea44196ab1df76e3b491a71f
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-04-28 15:18:44 -0400

    feat: add sheet component and button component

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,16 +1,38 @@
-import { Github, Linkedin } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Github, Linkedin, MenuIcon } from "lucide-react";
 import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="p-5">
-      <div className="container w-full flex flex-row gap-4">
-        <Link href="https://www.linkedin.com/in/rodrigo-moreira-santos/">
-          <Linkedin />
-        </Link>
-        <Link href="https://github.com/rodrigomoreirasantos">
-          <Github />
-        </Link>
+    <header className="py-5">
+      <div className="container w-full flex flex-row justify-between items-center">
+        <p>Rodrigo Moreira Santos</p>
+        <div className="flex flex-row gap-4 items-center">
+          <Link href="https://www.linkedin.com/in/rodrigo-moreira-santos/">
+            <Linkedin />
+          </Link>
+          <Link href="https://github.com/rodrigomoreirasantos">
+            <Github />
+          </Link>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline">
+                <MenuIcon />
+              </Button>
+            </SheetTrigger>
+
+            <SheetContent>
+              <SheetHeader>Menu</SheetHeader>
+              <Button></Button>
+            </SheetContent>
+          </Sheet>
+        </div>
       </div>
     </header>
   );
