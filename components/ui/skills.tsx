4bf7b8fcda2e8f7a8b0450103f83e9d9d35c0af9
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-04-29 10:08:01 -0400

    feat: add Skills

--- components/ui/skills.tsx
+++ components/ui/skills.tsx
@@ -13,7 +13,23 @@ const Skills = () => {
       />
       <Image
         src="/react.svg"
-        alt="Nextjs Icon"
+        alt="React Icon"
+        height={0}
+        width={0}
+        sizes="100vw"
+        className="h-10 w-10 object-contain"
+      />
+      <Image
+        src="/tailwindcss.svg"
+        alt="Tailwind Icon"
+        height={0}
+        width={0}
+        sizes="100vw"
+        className="h-10 w-10 object-contain"
+      />
+      <Image
+        src="/postgresql.svg"
+        alt="Tailwind Icon"
         height={0}
         width={0}
         sizes="100vw"
