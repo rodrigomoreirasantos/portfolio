ffca4eba11314c8c718c40fc782e2d1167d63105
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-04-29 08:51:16 -0400

    feat: add button About on Menu

---
+++ app/components/menu.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { MenuIcon } from "lucide-react";
+import Link from "next/link";
+
+const Menu = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline">
+          <MenuIcon />
+        </Button>
+      </SheetTrigger>
+
+      <SheetContent>
+        <SheetHeader className="text-left pb-5">Menu</SheetHeader>
+        <div className="flex flex-col gap-4">
+          <Link href="#About">
+            <SheetClose asChild>
+              <Button className="w-full">About</Button>
+            </SheetClose>
+          </Link>
+          {/* <Button className="w-full">Projects</Button> */}
+          {/* <Button className="w-full">Experience</Button> */}
+          {/* <Button className="w-full">Contact</Button> */}
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default Menu;
